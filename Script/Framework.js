fd96ce2d735a22327a032a06c95b1dad535b8663
Gustavo Laviola <gustavolaviolasanches@gmail.com>
2015-09-06 22:18:18 -0300

    pagina javascript com validacao

---
+++ Script/Framework.js
@@ -0,0 +1,48 @@
+﻿function onlyNumer(camp) {
+    var r = false;
+    r = isNotNull(camp);
+    if (r) {
+        r = isNumber(camp);
+    }
+
+    return r;
+}
+
+function isNotNull(camp) {
+    var valor = $(camp).value;
+    var erro = $("erro-" + camp);
+    if (valor == "") {
+        erro.style.backgroundColor = "red";
+        erro.style.padding = "5px";
+        erro.style.color = "#fff";
+        erro.innerHTML = "Este campo está vazio";
+        return false;
+    } else {
+        erro.style.backgroundColor = "#FFF";
+        erro.innerHTML = "";
+        return true;
+    }
+}
+
+function isNumber(camp) {
+    var valor = $(camp).value;
+    var erro = $("erro-" + camp);
+
+    var regex = /^[a-zA-Z]+$/;
+
+    if (valor.match(regex)) {
+        erro.style.backgroundColor = "red";
+        erro.style.padding = "5px";
+        erro.style.color = "#fff";
+        erro.innerHTML = "Este campo deve conter apenas numeros";
+        return false;
+    } else {
+        erro.style.backgroundColor = "#FFF";
+        erro.innerHTML = "";
+        return true;
+    }
+}
+
+var $ = function (campo) {
+    return document.getElementById(campo);
+}
---
+++ Script/Script.js
@@ -0,0 +1,13 @@
+﻿window.onload = function (e)
+{
+    document.getElementById("calcular").onclick = function fun()
+    {
+        var campo1SemErro = onlyNumer("primeiroNumero");
+        var campo2SemErro = onlyNumer("segundoNumero");
+        
+        if (campo1SemErro && campo2SemErro) {
+            var soma = parseInt($("primeiroNumero").value) + parseInt($("segundoNumero").value);
+            document.getElementById("retorno").innerHTML = soma;
+        }
+    }
+}
