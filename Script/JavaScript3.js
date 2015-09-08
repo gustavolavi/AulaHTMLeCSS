8f4a1657354c5b0e809df4e2754a3bae0900581e
Gustavo Laviola <gustavolaviolasanches@gmail.com>
2015-09-07 21:13:18 -0300

    adicao de pagina de javascript3 e exemplos de codigo javascript para validacao

---
+++ Script/JavaScript3.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+
+    document.getElementById("somar").onclick = function () {
+        var n1 = document.getElementById("numero1").value;
+        var n2 = document.getElementById("numero2").value;
+
+        if (n1.length < 1 || n2.length < 1) {
+            document.getElementById("resposta").innerHTML = "os valores devem conter mais de 1 numero";
+            document.getElementById("resposta").style.color = "#FFF";
+            document.getElementById("resposta").style.backgroundColor = "red";
+        } else {
+            var numero2 = parseInt(n2);
+            var numero1 = parseInt(n1);
+            var calculo = numero1 + numero2;
+            document.getElementById("resposta").innerHTML = calculo;
+            document.getElementById("resposta").style.color = "black";
+            document.getElementById("resposta").style.backgroundColor = "green";
+        }
+    }
+
+}
+
+function validarCaracteresMinimo(campo,numeroDeCaracteres)
+{
+    var c = document.getElementById(campo).value;
+    if (c.length >= numeroDeCaracteres) {
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ Script/scriptjs2.js
@@ -0,0 +1,45 @@
+﻿window.onload = function fun(e)
+{
+    $("somar").onclick = function () {
+        calculo("somar");
+    }
+
+    $("subtrair").onclick = function () {
+        calculo("subtrair");
+    }
+
+    $("dividir").onclick = function () {
+        calculo("dividir");
+    }
+}
+
+function $(campo){
+    return document.getElementById(campo);
+}
+
+function getValueNumber(campo)
+{
+    var r = $(campo).value;
+    return parseInt(r);
+}
+
+function calculo(tipo) {
+
+    var n1 = getValueNumber("numero1");
+    var n2 = getValueNumber("numero2");
+
+    switch (tipo) {
+        case "somar":
+            $("resposta").innerHTML = n1 + n2;
+            break;
+        case "subtrair":
+            $("resposta").innerHTML = n1 - n2;
+            break;
+        case "dividir":
+            $("resposta").innerHTML = n1 / n2;
+            break;
+        default:
+            $("resposta").innerHTML = "operação invalida";
+            break;
+    }
+}
